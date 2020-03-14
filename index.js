efd67c5f7404f8663b9ac8216ea03768f7cc9b38
lugovczov <vova.lugovczov@mail.ru>
2020-03-14 19:35:25 +0300

    feat: add off/active screen on phones

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ window.onload = function() {
 
     //top-nav
     addNavClickHandler();
-
+    //off-iphone
+    offScreenClickIphone();
 }
 
+//top-nav
 const addNavClickHandler = () => {
     document.querySelector('.nav').addEventListener('click', (e) => {
         if (e.target.classList.contains('top-nav__item')) {
@@ -29,4 +31,34 @@ const removeSelectedNav = () => {
 const selectedClickNav = (clickedNav) => {
     clickedNav.classList.add('nav-selected');
     clickedNav.classList.remove('nav-no-selected');
+}
+
+//off-iphone
+const offScreenClickIphone = () => {
+    let i = 0;
+    let j = 0;
+    
+    document.querySelector('.iphone-btn').addEventListener('click', () => {
+        let screen = document.querySelector('.off-screen');
+
+        if ( i == 0) {
+            screen.style.opacity = '1';
+            i++;
+        } else {
+            screen.style.opacity = '0';
+            i = 0;
+        }
+    })
+
+    document.querySelector('.iphone-btn_horizontal').addEventListener('click', () => {
+        let screen = document.querySelector('.off-screen_horizontal');
+
+        if ( j == 0) {
+            screen.style.opacity = '1';
+            j++;
+        } else {
+            screen.style.opacity = '0';
+            j = 0;
+        }
+    })
 }
