05ab0357935a64f3a8a09523b7cfa2593ff93191
lugovczov <vova.lugovczov@mail.ru>
2020-03-14 23:45:49 +0300

    feat: add slider

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ window.onload = function() {
     addNavClickHandler();
     //off-iphone
     offScreenClickIphone();
+
+    slider();
 }
 
 //top-nav
@@ -37,7 +39,7 @@ const selectedClickNav = (clickedNav) => {
 const offScreenClickIphone = () => {
     let i = 0;
     let j = 0;
-    
+
     document.querySelector('.iphone-btn').addEventListener('click', () => {
         let screen = document.querySelector('.off-screen');
 
@@ -59,6 +61,59 @@ const offScreenClickIphone = () => {
         } else {
             screen.style.opacity = '0';
             j = 0;
+        }
+    })
+}
+
+const slider = () => {
+    //slider
+    let items = document.querySelectorAll('.slider .slider-item');
+    let currentItem = 0;
+    //задержка для анимации 
+    let isEnabled = true;
+    
+    function changeCurrentItem(n) {
+        currentItem = (n + items.length) % items.length;
+    }
+    
+    function hideItem(direction) {
+        isEnabled = false;
+        items[currentItem].classList.add(direction);
+        items[currentItem].addEventListener('animationend', function () {
+            this.classList.remove('slider-active', direction);
+        });
+    }
+    
+    function showItem(direction) {
+        items[currentItem].classList.add('next', direction);
+        items[currentItem].addEventListener('animationend', function () {
+            this.classList.remove('next', direction);
+            this.classList.add('slider-active'); 
+            isEnabled = true;
+        });
+    }
+    
+    function previousItem(n) {
+        hideItem('to-right');
+        changeCurrentItem(n - 1);
+        showItem('from-left')
+    }
+    
+    function nextItem(n) {
+        hideItem('to-left');
+        changeCurrentItem(n + 1);
+        showItem('from-right');
+    }
+    
+    document.querySelector('.slider .ctrl-left').addEventListener('click', function() {
+        if (isEnabled) {
+            previousItem(currentItem);
+        }
+    })
+    
+    document.querySelector('.slider .ctrl-right').addEventListener('click', function() {
+        if (isEnabled) {
+            nextItem(currentItem);
         }
     })
 }
